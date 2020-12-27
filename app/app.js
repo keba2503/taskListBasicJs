b4834984aef9c5b10aecbc7f603f42d872461782
Karen Borrero <keba2503@gmail.com>
2020-12-27 14:35:51 +0100

    app, readme

--- app/app.js
+++ app/app.js
@@ -1,1 +1,32 @@
-console.log('prueba');
+//Variables
+const lista = document.querySelector("#lista");
+const  tareaInput = document.querySelector("#txtNota");
+const boton = document.querySelector("#boton");
+
+//Funciones
+function agregarTarea() {
+    
+};
+
+function comprobarInput() {
+   
+};
+
+function eliminarTarea() {
+   
+};
+
+
+//Eventos
+
+//Agregar Tarea
+boton.addEventListener("click", agregarTarea);
+
+//Comprobar input
+tareaInput.addEventListener("click", comprobarInput);
+
+//Eliminar tarea 
+for (let index = 0; index <= lista.children.length; index++) {
+    lista.children[index].addEventListener("click", eliminarTarea);
+}
+
